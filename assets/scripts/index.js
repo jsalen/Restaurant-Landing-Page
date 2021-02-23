528f1518fbe0b4a8456ee9aabf652735dd4098cf
Joseph Salen <jsalen.91@gmail.com>
2021-02-23 18:54:04 -0400

    Implemented Hamburger Animation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,22 @@
 const headerNav = document.getElementById("header-nav");
 const headerLogo = document.getElementById("header-logo");
+const hamburger = document.querySelector(".hamburger");
+
+function openHamburger() {
+	hamburger.classList.toggle("is-active");
+	toggleMenu();
+}
 
 function toggleMenu() {
 	if (headerNav.style.display === "block") {
 		headerLogo.style.transform = "rotate(-90deg)";
-		headerLogo.style.transition = "transform ease 0.2s";
+		headerLogo.style.transition = "transform 0.5s";
 		headerNav.style.display = "none";
+		headerNav.style.transition = "all 2s";
 	} else {
-		headerNav.style.display = "block";
 		headerLogo.style.transform = "rotate(0deg)";
-		headerLogo.style.transition = "transform ease 0.2s";
+		headerLogo.style.transition = "transform 0.5s";
+		headerNav.style.display = "block";
+		headerNav.style.transition = "all 2s";
 	}
 }
