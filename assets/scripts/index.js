97b4b56b4b3b8ca6c2cdc764fa424799120c32e6
Joseph Salen <jsalen.91@gmail.com>
2021-02-23 19:03:03 -0400

    changed JS a bit with prettier. No big changes.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,20 @@
-const headerNav = document.getElementById("header-nav");
-const headerLogo = document.getElementById("header-logo");
-const hamburger = document.querySelector(".hamburger");
+const headerNav = document.getElementById('header-nav')
+const headerLogo = document.getElementById('header-logo')
+const hamburger = document.querySelector('.hamburger')
 
-function openHamburger() {
-	hamburger.classList.toggle("is-active");
-	toggleMenu();
+const openHamburger = () => {
+	hamburger.classList.toggle('is-active')
+	toggleMenu()
 }
 
-function toggleMenu() {
-	if (headerNav.style.display === "block") {
-		headerLogo.style.transform = "rotate(-90deg)";
-		headerLogo.style.transition = "transform 0.5s";
-		headerNav.style.display = "none";
-		headerNav.style.transition = "all 2s";
+const toggleMenu = () => {
+	if (headerNav.style.display === 'block') {
+		headerLogo.style.transform = 'rotate(-90deg)'
+		headerLogo.style.transition = 'transform 0.5s'
+		headerNav.style.display = 'none'
 	} else {
-		headerLogo.style.transform = "rotate(0deg)";
-		headerLogo.style.transition = "transform 0.5s";
-		headerNav.style.display = "block";
-		headerNav.style.transition = "all 2s";
+		headerLogo.style.transform = 'rotate(0deg)'
+		headerLogo.style.transition = 'transform 0.5s'
+		headerNav.style.display = 'block'
 	}
 }
